596f735d59f982b1040ad9fa2c1b7039b586c1ca
Enlight1337 <bocharov6479@gmail.com>
2022-12-02 13:06:41 +0600

    day1 done

--- src/day1.clj
+++ src/day1.clj
@@ -9,15 +9,25 @@
           (partition-by empty?)
           (filter (comp not empty? first))))
 
-(defn aoc-day1
+(defn aoc-day1-2-list
      [input]
-     (apply max
-            (reduce
-                 (fn [coll val]
-                  (conj coll (apply + (map #(Integer/parseInt %) val))))
-                 [] input)))
+     (reduce
+          (fn [coll val]
+               (conj coll (apply + (map #(Integer/parseInt %) val))))
+          [] input))
+
+(defn aoc-day1-qst1
+     [list]
+     (apply max (aoc-day1-2-list list)))
+
+(defn aoc-day1-qst2
+     [list]
+     (apply +
+            (take-last 3
+                       (sort (aoc-day1-2-list list)))))
 
 (comment
-     (aoc-day1 day1-input))
+     (aoc-day1 day1-input)
+     (aoc-day2 day1-input))
 
 
