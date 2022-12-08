edda1c3f053424c4676d95f473575d1ac86e880b
Enlight1337 <bocharov6479@gmail.com>
2022-12-08 17:04:29 +0600

    day2 test

--- src/day2.clj
+++ src/day2.clj
@@ -1,1 +1,21 @@
 (ns day2)
+
+
+(def result-set
+  [{:a "scissors" :b "stone" :res 3}
+   {:a "stone" :b "scissors" :res 0}])
+
+
+(defn result
+  [x y]
+  (cond
+    (= x y) 1)
+  :else "lol")
+
+(comment
+  (get result-set :a)
+  (:res (first  (filter #(= (:a %) "scissors") result-set)))
+
+  (:a {:a "scissors" :b "stone" :res 3})
+  )
+
